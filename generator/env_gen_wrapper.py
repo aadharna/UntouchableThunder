2491b8b11d1d2d57dacf1cc390bdfd831656b9c7
aadharna <adharna@fordham.edu>
2019-11-12 16:52:48 -0500

    update generator and env wrapper to allow for transer testing

--- generator/env_gen_wrapper.py
+++ generator/env_gen_wrapper.py
@@ -19,23 +19,40 @@ class GridGame(gym.Wrapper):
                  mechanics=[],
                  locations={},
                  gen_id=0,
-                 images=False):
+                 images=False,
+                 shape=(9, 13)):
 
         """Returns Grid instead of pixels
         Sets the reward
         Generates new level on reset
         --------
         """
-        self.name = game
+        self.game = game
         self.dir_path = path # gvgai.dir path + envs/games/zelda_v0
         self.lvl_path = os.path.join(path, lvl_name)
         self.mechanics = mechanics
-
-        self.generator = Generator(tile_world=_initialize(self.lvl_path),
-                                   path=path,
-                                   mechanics=self.mechanics,
-                                   generation=gen_id,
-                                   locations=locations)
+        
+        if not bool(locations):
+            #set up first level, read it in from disk.
+            lvl = _initialize(self.lvl_path)
+            self.generator = Generator(tile_world=lvl,
+                                       shape=lvl.shape,
+                                       path=path,
+                                       mechanics=self.mechanics,
+                                       generation=gen_id,
+                                       locations=locations)
+        else:
+            # use generated lvl contained within locations dict.
+            self.generator = Generator(tile_world=None,
+                                       shape=shape,
+                                       path=path,
+                                       mechanics=self.mechanics,
+                                       generation=gen_id,
+                                       locations=locations)
+        # save to disk, can comment out if that lets me multithread.
+        self.generator.to_file(GridGame.env_count, self.game)
+        
+        # return picture states?
         self.pics = images
         
         # self.env = gym_gvgai.make('gvgai-{}-lvl0-v0'.format(game))
@@ -47,7 +64,6 @@ class GridGame(gym.Wrapper):
         # update static count of number of all envs
         self.id = GridGame.env_count
         GridGame.env_count += 1
-        gym.Wrapper.__init__(self, self.env)
 
         self.depth = None # gets set in self.reset()
         # env must exist to reset
@@ -66,7 +82,7 @@ class GridGame(gym.Wrapper):
         self.score = 0
         # save file currently in generator to disk
         s = str(self.generator)
-        self.env.reset(environment_id=f'{self.name}-custom', level_data=s)
+        self.env.reset(environment_id=f'{self.game}-custom', level_data=s)
         if self.pics:
             (pix, state), _, _, _ = self.env.step(0)
         else:
@@ -74,6 +90,7 @@ class GridGame(gym.Wrapper):
         state = np.transpose(state, (2, 0, 1))
         if self.depth is None:
             self.depth = state.shape[0]
+        # print(state)
         return state
 
     def step(self, action):
@@ -93,25 +110,32 @@ class GridGame(gym.Wrapper):
         return state, reward, done, {'pic': im}
 
     def mutate(self, mutationRate):
-        new_map = self.generator.mutate(mutationRate)
+        new_map, shape = self.generator.mutate(mutationRate)
 
         childGG = GridGame(game=self.game,
                              play_length=self.play_length,
                              path=self.dir_path,
-                             lvl_name=self.lvl_path,
+                             lvl_name=f"{self.game}_id:{self.id}_g:{self.generator.generation+1}.txt",
                              gen_id=self.generator.generation + 1,
                              mechanics=self.mechanics,
-                             locations=new_map)
+                             images=self.pics,
+                             locations=new_map,
+                             shape=shape)
 
         return childGG
 
 
     def fitness(self, agent):
-        """Score agent by having it try to complete the level.
+        """Score THIS agent by having it try to complete THIS level.
+        
+        This function allows you to easily test any agent on an env that it's not paired with.
+        (or it can be used to test an agent with it's own paired env if called through the agent's fitness fn).
+        
+        To test a non-paired agent with this env, call this function directly: PAIR.env.fitness(new_agent)
+        
         :param agent: (NN-)agent
         :return:
         """
-        print("running agent")
-        score = np.sum(agent.evaluate())
-        self._fit = score / 100
+        # print(f"testing env {self.id} on agent {agent.id}")
+        return np.sum(agent.evaluate(self))
 
