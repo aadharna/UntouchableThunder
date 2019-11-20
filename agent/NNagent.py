6a5a3cec76685592e1b23a39ea68c1faf6f47bdb
aadharna <adharna@fordham.edu>
2019-11-20 17:50:31 -0500

    edit NN architecture to include compass information

--- agent/NNagent.py
+++ agent/NNagent.py
@@ -16,17 +16,18 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=depth, out_channels=8, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3)
-        self.fc1 = nn.Linear(32 * 3, 48)
+        self.fc1 = nn.Linear((32 * 3) + 4, 48)
         self.fc2 = nn.Linear(48, 24)
         self.fc3 = nn.Linear(24, n_actions)
 
-    def forward(self, x):
+    def forward(self, x, compass=torch.zeros(4, dtype=torch.float64)):
         x = self.pool(F.relu(self.conv1(x)))
         x = F.relu(self.conv2(x))
-        x = x.view(-1, 32 * 3)
+        x = x.view(32 * 3)
+        x = torch.cat((x, compass))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x), dim=1) #dim=0 --> [1, 1, 1, 1, ..., 1]
+        x = F.softmax(self.fc3(x), dim=0) #dim = 0 since array was flattened
         return x
 
 class NNagent(Agent):
@@ -62,9 +63,11 @@ class NNagent(Agent):
         """
         # the grid needs to be part of a 'batch', so we make state the only element in a list.
         inp = Variable(torch.from_numpy(np.array([state])), requires_grad=False)
+        # get one-hot encoding of the direction the agent is facing. note: self.env.orientation is 0-indexed
+        compass = Variable(torch.from_numpy(self.env.orientation[self.env.prev_move - 1]), requires_grad=False)
         with torch.no_grad():
-            outputs = self.nn(inp.double())
-            _, predicted = torch.max(outputs, 1)
+            outputs = self.nn(inp.double(), compass.double())
+            _, predicted = torch.max(outputs, 0)
         # break data out of tensor
-        return predicted.data.numpy()[0]
+        return predicted.data.numpy()
 
