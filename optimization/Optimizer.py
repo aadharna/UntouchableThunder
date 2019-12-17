b6d0cd5e194d7fb69fba73ebaae5728eed049c49
aadharna <adharna@fordham.edu>
2019-12-17 12:15:41 -0500

    update Optimizer class; test other GAs

--- optimization/Optimizer.py
+++ optimization/Optimizer.py
@@ -1,13 +1,15 @@
 import torch
+import ctypes
 import numpy as np
 from functools import reduce
 from collections import OrderedDict
 from utils.diff_evo import differential_evolution
+from devo import *
 
-class PyTorchObjective(object):
+class PyTorchObjective():
     """PyTorch objective function, wrapped to be called by scipy.optimize."""
 
-    def __init__(self, agent, bound_limits=(-5, 5)):
+    def __init__(self, agent, bound_limits=(-5, 5), popsize=100):
         self.f = agent.nn  # some pytorch module
         # make an x0 from the parameters in this module
         parameters = OrderedDict(agent.nn.named_parameters())
@@ -22,9 +24,16 @@ class PyTorchObjective(object):
         self.id = agent.name
         self.c = 0
         self.bounds = [bound_limits]*self.x0.shape[0]
+        self.popsize = popsize
+        
+        self.init_fitnesses = np.random.rand(popsize, 1)
+        self.out_population = np.zeros((popsize, self.x0.shape[0]), dtype='float32')
+        self.out_fitnesses  = np.zeros((popsize, 1), dtype='float32')
+        
+        
 
     def unpack_parameters(self, x):
-        """optimize.minimize will supply 1D array, chop it up for each parameter."""
+        """will be supplied a 1D array, chop it up for each parameter."""
         i = 0
         named_parameters = OrderedDict()
         for n in self.param_shapes:
@@ -67,19 +76,77 @@ class PyTorchObjective(object):
         score = self.eval_fn()
         self.cached_score = score
 
-    def fun(self, x):
+    def fun(self, x, dimension):
         self.c += 1
-        if self.is_new(x):
-            self.cache(x)
-        if self.c % 150 == 0:
-            print(f"achieved score of: {self.cached_score} on {self.c}")
+        z = np.zeros(dimension, dtype=np.float32)
+        for i in range(dimension):
+            z[i] = x[i]
+        if self.is_new(z):
+            self.cache(z)
+            
         return self.cached_score
-
+    
+    def fun_c(self, x, dimension):
+        self.c += 1
+        z = np.zeros(dimension, dtype=np.float32)
+        for i in range(dimension):
+            z[i] = x[i]
+        if self.is_new(z):
+            self.cache(z)
+        
+        return ctypes.c_double(self.cached_score)
+        
+        
     def update_nn(self, answer):
         state_dict = self.unpack_parameters(answer.x)
         self.f.load_state_dict(state_dict)
 
+    def create_population(self):
+        ## TALK WITH TAE JONG ABOUT A GOOD WAY TO ADD NOISE.
+        
+        # get noise
+        noise = np.random.randn(self.popsize, self.x0.shape[0])
+        # add noise to theta for each member of the population
+        pop = [self.x0 + noise[i] for i in range(self.popsize)]
+        return np.array(pop)
+    
+    def results_callback(self, population, fitness_values, population_size, problem_size):
+        # Store results to python memory containers
+        # Store population
+        for i in range(0, population_size * problem_size):
+            row = int(i / problem_size)
+            col = i % problem_size
+            self.out_population[row][col] = population[i]
 
+        # Store fitness values
+        for j in range(0, population_size):
+            f = fitness_values[j]
+            self.out_fitnesses[j] = f
+        return
+        
+
+    
+    
+def run_TJ_DE(_de, pair, n=3, popsize=99):
+    """run Tae Jong's DE
+    _de:  which DE algorithm do we want to use?
+    pair: PyTorchObjective wrapper of paired NNAgent_Environment object
+    n: number of function evaluations
+    """
+    return _de.run(
+                    n,
+                    pair.popsize,
+                    0.5,
+                    0.9,
+                    pair.fun,
+                    pair.x0.shape[0],
+                    -5.0,
+                    5.0,
+                    pair.create_population().ctypes.data_as(c.POINTER(c.c_double)),
+                    pair.init_fitnesses.ctypes.data_as(c.POINTER(c.c_double)),
+                    pair.results_callback
+                    )
+    
 def run_opt_n_steps(pair, n=3, popsize=99, strategy='rand1bin'):
     """run n steps on the evolution optimizer
 
