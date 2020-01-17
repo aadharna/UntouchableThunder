d35feff0cf38cf71a589326b8aed6cc37d962915
aadharna <adharna@fordham.edu>
2020-01-17 14:48:09 -0500

    add scripts to govern behavior between processes

--- child.py
+++ child.py
@@ -1,1 +1,25 @@
-from utils.ADPChild import ADPChildRunner
+import time
+from utils.ADPChild import ADPChild
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--id", type=int, help='set child id')
+args = parser.parse_args()
+
+if __name__ == "__main__":
+
+    child = ADPChild(args.id)
+
+    while True:
+        try:
+            while not child.hasTask():
+                time.sleep(5)
+            print("found task")
+            child.recieveTaskAndReturnAnswer()
+
+        except KeyboardInterrupt as e:
+            print(e)
+            child.pair.env.close()
+            import sys
+            sys.exit(0)
--- parent.py
+++ parent.py
@@ -1,33 +1,63 @@
+import time
+import argparse
+
 from utils.ADPParent import ADPParent
 from utils.ADPTASK_ENUM import ADPTASK
 
 from agent.NNagent import NNagent
 from generator.env_gen_wrapper import GridGame
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("n", type=int, default=1)
-args = parser.parse_args()
-
-
-parent = ADPParent(args.n)
-
-
-pair = NNagent(GridGame(game='zelda',
-                        play_length=250,
-                        path='./levels',
-                        lvl_name='start.txt',
-                        mechanics=['+', 'g'],
-                        # monsters, key, door, wall
-                        )
-               )
-
-parent.registerChildren()
-
-
-parent.createChildTask(pair.nn, pair.env, ADPTASK.EVALUATE)
+def callOut(parent):
+    print("calling out")
+    children = []
+    while len(children) < 1:
+        print("calling out again")
+        try:
+            time.sleep(5)
+        except KeyboardInterrupt as e:
+            print('dying')
+            print(e)
+            import sys
+            sys.exit(0)
+        children = parent.pickupChildren()
+    print(f'children alive: {len(children)}')
+    return children
 
 
 
 
-pair.env.close()
+
+
+if __name__ == "__main__":
+
+    parent = ADPParent()
+
+
+    pair = NNagent(GridGame(game='zelda',
+                            play_length=250,
+                            path='./levels',
+                            lvl_name='start.txt',
+                            mechanics=['+', 'g'],
+                            # monsters, key, door, wall
+                            )
+                   )
+
+    while True:
+        try:
+            children = callOut(parent)
+            print(children)
+            parent.createChildTask(pair.nn, pair.env, ADPTASK.EVALUATE, children[0], rl=True)
+            break
+        except KeyboardInterrupt as e:
+            print(e)
+            pair.env.close()
+            import sys
+            sys.exit(0)
+
+
+
+    pair.env.close()
+
+
+
+
