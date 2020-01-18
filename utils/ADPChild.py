b210497a2026b350d3dba67bbff7b2529a5865df
aadharna <adharna@fordham.edu>
2020-01-17 22:01:20 -0500

    allow child to optimize NNs

--- utils/ADPChild.py
+++ utils/ADPChild.py
@@ -1,7 +1,11 @@
 import os
 from agent.NNagent import NNagent
 from generator.env_gen_wrapper import GridGame
+from optimization.Optimizer import PyTorchObjective
 from utils.ADPTASK_ENUM import ADPTASK
+
+from optimization.runners import run_TJ_DE, run_ppo
+# from devo import jDE, DE, CoDE
 
 from utils.utils import save_obj, load_obj
 
@@ -28,7 +32,7 @@ class ADPChild:
 
         self.placeChildFlag(self.alive)
         print(f"child {self.id} alive signal sent")
-        self.placeChildFlag(self.busy)
+        # self.placeChildFlag(self.busy)
 
         self.args = {}
 
@@ -43,6 +47,11 @@ class ADPChild:
                         )
                )
 
+    ########### END CONSTRUCTOR #############
+
+
+
+
     def hasTask(self):
         path = os.path.join(self.root,
                             self.subfolders['sent_by_parent'],
@@ -52,7 +61,7 @@ class ADPChild:
             return True
         return False
 
-
+    # create folders if parent has failed to do so.
     def createFolders(self):
         for f in self.subfolders.keys():
             path = os.path.join(self.root, self.subfolders[f])
@@ -64,19 +73,41 @@ class ADPChild:
             pass
 
     def doTask(self, taskID):
+        """
+        Execute the asked for task
+
+        :param taskID: EVALUATE the NN or OPTIMIZE it
+        :return: result dict
+        """
+        rl = False
+        if hasattr(self.args, 'rl'):
+            rl = self.args['rl']
+
         if taskID == ADPTASK.EVALUATE:
-            rl = False
-            if hasattr(self.args, 'rl'):
-                rl = self.args['rl']
             score = self.pair.evaluate(rl=rl)
             return {
                 'chromosome_id': self.chromosome_id,
                 'score': score,
             }
+
         elif taskID == ADPTASK.OPTIMIZE:
             # run optimization here
-
+            if rl:
+                # optimizes in place
+                run_ppo(policy_agent       = self.pair,
+                        env_fn             = self.pair.env.make,
+                        path               = './runs',
+                        n_concurrent_games = 1,
+                        frames             = 100000)
+            else:
+                objective = PyTorchObjective(agent=self.pair)
+                # run_TJ_DE(_de=devo.jDE,
+                #           pair=objective,
+                #           n=10000)
+                # objective.update_nn(objective.best_individual)
+            score = self.pair.evaluate(rl=rl)
             return {
+                'score': score,
                 'chromosome_id': self.chromosome_id,
                 'nn': self.pair.nn.state_dict()
             }
@@ -89,11 +120,16 @@ class ADPChild:
 
         task_params = load_obj(path, f'child{self.id}.pkl')
         os.remove(os.path.join(path, f'child{self.id}') + '.pkl')
+
+        # update network and env to execute on task
         self.pair.env.generator.update_from_lvl_string(task_params['lvl'])
         self.pair.nn.load_state_dict(task_params['nn'])
+        # save id of PAIR to pass back
         self.chromosome_id = task_params['chromosome_id']
+        # get additional arguments passed in
         self.args.update(task_params['kwargs'])
 
+        # execute the asked for task
         return self.doTask(task_params['task_id'])
 
     def returnAnswer(self, answer):
@@ -108,6 +144,7 @@ class ADPChild:
         os.remove(self.busy)
 
     def __del__(self):
-        self.pair.env.close()
         os.remove(self.alive)
         os.remove(self.busy)
+        self.pair.env.close()
+
