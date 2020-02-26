b95170e535754375718a46360f63cbef694f5a4e
aadharna <adharna@fordham.edu>
2020-02-26 01:28:33 -0500

    feed in init_pop; add argument to obj_fn; pass in PyTObj pair; pass in dict to hold scores

--- utils/CoDE.py
+++ utils/CoDE.py
@@ -1,5 +1,7 @@
-from utils.DE import DE_individual, best_fitness, rand_1_bin, selection, init_pop
 import numpy as np
+from tqdm import tqdm
+
+from utils.DE import DE_individual, best_fitness, rand_1_bin, selection, init_pop
 
 def rand_2_bin(pop, iteration, mutationRate, scaling_factor, problem_size, _min, _max):
     # V_i, g = X_{r1, g} + F * (X_{r2, g} - X_{r3, g}) + F * (X_{r4, g} - X_{r5, g})
@@ -32,28 +34,37 @@ def current_to_rand_1(pop, iteration, mutationRate, scaling_factor, problem_size
 
     return trial
 
-def CoDE(obj_fn,
-         pop_size,
+def CoDE(pair,
+         init_population,
          problem_size,
          scaling_factor, #list of three numbers
          crossover_rate, #list of three numbers
          lower_bound,
          upper_bound,
-         generation_max):
+         generation_max,
+         scores=None):
+    
+    obj_fn = pair.obj_fun
+    
+    if scores is None:
+        scores = {}
+    pop_size = pair.popsize
+    
     scaling_factor = [1.0, 1.0, 0.8]
     crossover_rate = [0.1, 0.9, 0.2]
+    
+    np.clip(init_population, lower_bound, upper_bound, out=init_population)
 
-    population = [DE_individual(np.zeros(problem_size)) for _ in range(pop_size)]
+    population = [DE_individual(p) for p in init_population]
     candidates = [DE_individual(np.zeros(problem_size)) for _ in range(pop_size)]
 
     solution = 0
 
-    init_pop(population, lower_bound, upper_bound)
-    for individual in population:
-        individual.fitness = obj_fn(individual.x)
+    for individual in tqdm(population):
+        individual.fitness = obj_fn(individual.x, problem_size)
 
     for generation in range(generation_max):
-        for j in range(pop_size):
+        for j in tqdm(range(pop_size)):
             trial1 = rand_1_bin(population, j,
                                    crossover_rate[0],
                                    scaling_factor[0],
@@ -71,13 +82,15 @@ def CoDE(obj_fn,
                                        lower_bound, upper_bound)
 
             _1 = DE_individual(trial1)
-            _1.fitness = obj_fn(trial1)
+            _1.fitness = obj_fn(trial1, problem_size)
             _2 = DE_individual(trial2)
-            _2.fitness = obj_fn(trial2)
+            _2.fitness = obj_fn(trial2, problem_size)
             _3 = DE_individual(trial3)
-            _3.fitness = obj_fn(trial3)
+            _3.fitness = obj_fn(trial3, problem_size)
 
-            best_trial = sorted([_1, _2, _3], key=lambda x: x.fitness)[0]
+            trial_vecs = [_1, _2, _3]
+
+            best_trial = sorted(trial_vecs, key=lambda x: x.fitness)[0]
 
             candidates[j].x = best_trial.x
             candidates[j].fitness = best_trial.fitness
@@ -86,11 +99,13 @@ def CoDE(obj_fn,
         for k, c in enumerate(candidates):
             selection(population[k], c)
 
+        scores[generation] = [p.fitness for p in population]
+
     solution += best_fitness(population)
-    print(solution)
+    # print(solution)
     return sorted(population, key=lambda x: x.fitness)[0].x
 
-def sphere(x):
+def sphere(x, dim):
     return np.dot(x, x)
 
 if __name__ == "__main__":
