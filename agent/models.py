074a61834c3349c61819b7b683594363ed9c39d3
aadharna <adharna@fordham.edu>
2020-03-02 16:33:53 -0500

    make network flexible to dzelda and solarfox

--- agent/models.py
+++ agent/models.py
@@ -25,10 +25,11 @@ class Value(nn.Module):
 class Net(nn.Module): 
     def __init__(self, n_actions, depth):
         super(Net, self).__init__()
+        self.resizecoeff = 3 if depth == 13 else 4
         self.conv1 = nn.Conv2d(in_channels=depth, out_channels=8, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3)
-        self.fc1 = nn.Linear((32 * 3) + 4, 48)
+        self.fc1 = nn.Linear((32 * self.resizecoeff) + 4, 48)
                         # neurons from conv layer + neurons to enter compass info
         self.fc2 = nn.Linear(48, 24)
         self.fc3 = nn.Linear(24, n_actions)
@@ -36,7 +37,7 @@ class Net(nn.Module):
     def forward(self, x, compass):
         x = self.pool(F.relu(self.conv1(x)))
         x = F.relu(self.conv2(x))
-        x = x.view(len(x), 32 * 3)
+        x = x.view(len(x), 32 * self.resizecoeff)
         x = torch.cat([x, compass], dim=1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
