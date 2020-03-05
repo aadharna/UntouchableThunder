1cfff613eccfd7a8364386b68e0f8163356802b5
aadharna <adharna@fordham.edu>
2020-03-05 00:24:58 -0500

    pass in exp-name to child.py

--- run_exp.sh
+++ run_exp.sh
@@ -5,12 +5,12 @@ mkdir -p ./../results_$1
 mkdir -p ./../results_$1/logs
 cp ./args.yml ./../results_$1/
 
-source activate thesis; python -u poet_distributed.py --exp_name $arg > ./../results_$1/logs/master.log 2>&1 &
+python -u poet_distributed.py --exp_name $arg > ./../results_$1/logs/master.log 2>&1 &
 
 sleep 1
 
-for((i=0; i<10; i++)); do
-    source activate thesis; OMP_NUM_THREADS=1 python -u child.py --id $i > ./../results_$1/logs/w$i.log 2>&1 &
+for((i=0; i<1; i++)); do
+    OMP_NUM_THREADS=1 python -u child.py --id $i --exp_name $arg > ./../results_$1/logs/w$i.log 2>&1 &
     sleep 2
 done
 
