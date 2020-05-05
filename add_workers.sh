8a8930704a65ea17658777c0227b9330cdf95461
aadharna <adharna@fordham.edu>
2020-05-04 20:00:27 -0400

    reinstate starting network; remove 1 add_worker argument

--- add_workers.sh
+++ add_workers.sh
@@ -2,7 +2,7 @@
 
 #hello
 
-for((i=31; i<35; i++)); do
-	    OMP_NUM_THREADS=1 python -u child.py --id $i --exp_name $1 --args_file $2 > ./../results_$1/logs/w$i.log 2>&1 &
+for((i=22; i<23; i++)); do
+	    OMP_NUM_THREADS=1 python -u child.py --id $i --exp_name $1 --args_file ./../results_$1/args.yml > ./../results_$1/logs/w$i.log 2>&1 &
 	    sleep 1
 done
