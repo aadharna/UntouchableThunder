2e84bf8855284ec6cd1c681412a563d7c7651dac
aadharna <adharna@fordham.edu>
2020-05-23 15:32:57 -0400

    Add in limited mcts function; test level difficulty

--- utils/call_java_competition_agent.py
+++ utils/call_java_competition_agent.py
@@ -6,6 +6,7 @@ def _jarWrapper(*args):
     # keep the script holding the processes
     while process.poll() is None:
         line = process.stdout.readline()
+        
         if len(line) == 0: #don't add in blanks ''
             continue
         ret.append(line[:-1])
@@ -29,3 +30,32 @@ def runJavaAgent(jar, vgdl, lvl, agent, length):
     
     # boolean. Did the agent win the level
     return float(result['win']) == 1
+
+
+import os
+from tqdm import tqdm
+
+def _jarLimitedMCTS(*args):
+    process = Popen(['java', '-jar']+list(args), stdout=PIPE, stderr=PIPE)
+    ret = []
+    # keep the script holding the processes
+    while process.poll() is None:
+        pass
+    return
+
+
+def limited_mcts(jar, vgdl, lvl, path, treesize, length):
+    for d in tqdm(sorted(os.listdir('.'))): 
+        if os.path.isdir(d): 
+            try: 
+                _net = int(d) 
+            except ValueError as e: 
+                continue 
+            for f in os.listdir(f'./{_net}'): 
+                if f'lvl' in f:
+                    lvl_id = f.split(".")[0].split('l')[-1]
+                    args = [jar, vgdl, f"{_net}/{f}", f"mcts{treesize}/{_net}.json", treesize, length]
+                    result = _jarLimitedMCTS(*args)
+                    break
+
+
