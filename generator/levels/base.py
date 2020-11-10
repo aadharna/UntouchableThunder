5e0cbe77599160d1b35d082432640f73452318f9
aadharna <adharna@fordham.edu>
2020-11-10 14:50:27 -0500

    handle file IO errors in one place; wait until the file is created to read it

--- generator/levels/base.py
+++ generator/levels/base.py
@@ -1,9 +1,8 @@
 import numpy as np
+import time
 
 class BaseGenerator:
     def __init__(self):
-
-
         pass
 
     # for compatability reasons
@@ -29,13 +28,29 @@ def _initialize(path, d=9):
     :param path: path to txt file representation of level
     :return:
     """
-    with open(path, 'r') as file:
-        f = file.read()
-        if f[-1] != "\n":
-            f += "\n"
-        f = f.split('\n')[:-1] #remove blank line.
-        d = len(f)
-        tile = [list(row) for row in f]
+    time.sleep(0.05)
+    failing = True
+    while failing:
+
+        fpointer = open(path, 'r')
+        try:
+            f = fpointer.read()
+            t = f[-1]
+            failing = False
+        except IndexError:
+            time.sleep(0.1)
+        finally:
+            fpointer.close()
+    time.sleep(0.05)
+    # with open(path, 'r') as file:
+    #     # print(path)
+    #     f = file.read()
+
+    if f[-1] != "\n":
+        f += "\n"
+    f = f.split('\n')[:-1] #remove blank line.
+    d = len(f)
+    tile = [list(row) for row in f]
     
     npa = np.array(tile).reshape((d, -1))  # make into numpy array 9x13
     return npa
