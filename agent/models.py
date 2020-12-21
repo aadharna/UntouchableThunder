ecca078c25058ce59aab9cb3719fba1471adfc95
aadharna <aadharna@gmail.com>
2020-12-21 17:00:09 -0500

    update model params for new sprite

--- agent/models.py
+++ agent/models.py
@@ -25,7 +25,7 @@ class Value(nn.Module):
 class Net(nn.Module): 
     def __init__(self, n_actions, depth):
         super(Net, self).__init__()
-        self.resizecoeff = 3 if depth == 5 else 4
+        self.resizecoeff = 3 if depth == 6 else 4
         self.conv1 = nn.Conv2d(in_channels=depth, out_channels=8, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=3)
